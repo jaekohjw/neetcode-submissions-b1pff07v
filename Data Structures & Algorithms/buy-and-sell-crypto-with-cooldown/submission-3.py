d2acdefb9aa55bbd105b0cb42dc9dbd6e8dfeedc
jaekohjw <30973357+jaekohjw@users.noreply.github.com>
2026-04-15 01:26:36 +0800

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        Held = [0] * n
+        Sold = [0] * n
+        Free = [0] * n
+        Held[0] = - prices[0]
+
+        for i in range(1, n):
+            Held[i] = max(Held[i - 1], Free[i - 1] - prices[i])
+            Sold[i] = Held[i - 1] + prices[i]
+            Free[i] = max(Free[i - 1], Sold[i - 1])
+        
+        return max(Sold[n - 1], Free[n - 1])
+
+
+
+
+        
