5d3f9287fdcf54e057da1443f199f43edf3f3cab
jaekohjw <30973357+jaekohjw@users.noreply.github.com>
2026-04-15 23:33:20 +0800

    Add: regular-expression-matching - submission-0

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-0.py
@@ -0,0 +1,29 @@
+from functools import cache
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+
+        @cache
+        def isMatch(m, n):
+            if n < 0:
+                return m < 0
+            if m < 0:
+                if p[n] == "*":
+                    return isMatch(m, n - 2)
+                return False
+            
+            
+            if p[n] == ".":
+                return isMatch(m - 1, n - 1)
+            if p[n] == "*":
+                preChar = p[n - 1]
+                idx = m 
+                while idx >= 0 and (preChar == "." or s[idx] == preChar):
+                    if isMatch(idx, n - 1):
+                        return True
+                    idx -= 1
+                return isMatch(m, n - 2)
+            return s[m] == p[n] and isMatch(m - 1, n - 1)
+        
+        return isMatch (len(s) - 1, len(p) - 1)
+
+        
