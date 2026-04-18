e73ac9f58904a3d4e71aebbe4282559cf48d8877
jaekohjw <30973357+jaekohjw@users.noreply.github.com>
2026-04-18 18:32:03 +0800

    Add: swim-in-rising-water - submission-2

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-2.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+
+        def bfs(t):
+            visited = set()
+            q = deque([(0, 0)])
+
+            while q:
+                r, c = q.popleft()
+                if r == N - 1 and c == N - 1:
+                    return True
+
+                directions = [(0,1),(0,-1),(1,0),(-1,0)]
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in visited and grid[nr][nc] <= t:
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+            
+            return False 
+
+        max_h = max(max(row) for row in grid)
+
+        lo = max(grid[0][0], grid[N - 1][N - 1])
+        hi = max_h
+        
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if bfs(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+            
+        return lo
+
+            
+        
